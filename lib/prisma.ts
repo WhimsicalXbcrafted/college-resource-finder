a54dd19ed8e25ed446d58ac6f230f8bf8a322425
WhimsicalXbcrafted <tangphong06@gmail.com>
2025-05-22 21:52:33 -0700

    IM DONE

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,22 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: ['query'],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL
-      }
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+export const prisma = global.prisma || new PrismaClient({
+  log: ['query'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
     }
-  })
-}
+  }
+})
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
-}
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
